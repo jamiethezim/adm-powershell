4a52088693afd65ac4dc54a2218899b4a0388122
Jamie Zimmerman <jamie_zimmerman@m276826xfgtfj.symc.symantec.com>
2017-09-06 16:19:34 -0700

    skeleton framework of downloading and editing files

--- azps.ps1
+++ azps.ps1
@@ -1,7 +1,8 @@
 Param(
    [string]$RGname,
    [string]$SAname,
-   [string]$Container
+   [string]$ContainerName,
+   [string]$DestinationFolder
 )
 Write-Host "Executing pwd";
 $pwd = pwd;
@@ -14,4 +15,30 @@ Write-Host "resource group is $RGname";
 Write-Host "storage account is $SAname";
 $SAKey = Get-AzureRmStorageAccountKey -ResourceGroupName "$RGname" -AccountName "$SAname";
 $Context = New-AzureStorageContext -StorageAccountName $SAname -StorageAccountKey $SAKey.Value[0];
-Write-Host $Context;
+
+#List all blobs in a container.
+$blobs = Get-AzureStorageBlob -Container $ContainerName -Context $Context;
+#Download blobs from a container.
+New-Item -Path $DestinationFolder -ItemType Directory -Force;
+$blobs | Get-AzureStorageBlobContent -Destination $DestinationFolder -Context $Context;
+
+
+##################################################################################################
+#Now we edit the files locally
+$pathToRollout = $DestinationFolder + "rolloutspec_multi-region.json";
+$pathtoServiceModel = $DestinationFolder + "servicemodel.json";
+
+$rollout = Get-Content $pathToRollout | ConvertFrom-Json;
+$servicemodel = Get-Content $pathtoServiceModel | ConvertFrom-Json;
+
+#TODO: make tag changes here based on user inputs!!!!
+#
+#
+#
+#
+
+$rollout | ConvertTo-Json | set-content $pathToRollout;
+$servicemodel | ConvertTo-Json | set-content $pathtoServiceModel;
+
+##################################################################################################
+#Now we upload the edited files back to storage account
