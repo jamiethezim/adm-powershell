dfe2fef1ffabb6a16365d1220a4dbc23b551a5d0
Jamie Zimmerman <jamiez@uoregon.edu>
2017-09-08 13:51:17 -0700

    Update azps.ps1

--- azps.ps1
+++ azps.ps1
@@ -36,9 +36,28 @@ $servicemodel = Get-Content $pathtoServiceModel | ConvertFrom-Json;
 # $rollout.RolloutMetadata.Name = RolloutMetadataName; #where variable RolloutMetadataName is an argument passed into the script
 # and processed in the PARAM section
 #
-#
-#
-#
+# rolloutspec_multi-region.json
+#(Get-Content $pathToRollout).replace("{{RollOutMetadata.Name}}", $RollOutMetadata_Name) | Set-Content $pathToRollout 
+#(Get-Content $pathToRollout).replace("{{RollOutMetadata.BuildSource.Parameters.VersionFile}}", $RollOutMetadata_BuildSource_Parameters_VersionFile) | Set-Content $pathToRollout 
+#(Get-Content $pathToRollout).replace("{{RollOutMetadata.Notification.Email.To}}", $RollOutMetadata_Notification_Email_To) | Set-Content $pathToRollout 
+#(Get-Content $pathToRollout).replace("{{OrchestratedSteps.RolloutCheck.Group1.Name}}", $OrchestratedSteps_RolloutCheck_Group1_Name) | Set-Content $pathToRollout 
+#(Get-Content $pathToRollout).replace("{{OrchestratedSteps.RolloutCheck.Group1.TargetName}}", $OrchestratedSteps_RolloutCheck_Group1_TargetName) | Set-Content $pathToRollout 
+#(Get-Content $pathToRollout).replace("{{OrchestratedSteps.RolloutCheck.Group2.Name}}", $OrchestratedSteps_RolloutCheck_Group2_Name) | Set-Content $pathToRollout 
+#(Get-Content $pathToRollout).replace("{{OrchestratedSteps.RolloutCheck.Group2.TargetName}}", $OrchestratedSteps_RolloutCheck_Group2_TargetName) | Set-Content $pathToRollout 
+#(Get-Content $pathToRollout).replace("{{OrchestratedSteps.RolloutCheck.Group3.Name}}", $OrchestratedSteps_RolloutCheck_Group3_Name) | Set-Content $pathToRollout 
+#(Get-Content $pathToRollout).replace("{{OrchestratedSteps.RolloutCheck.Group3.TargetName}}", $OrchestratedSteps_RolloutCheck_Group3_TargetName) | Set-Content $pathToRollout 
+
+#servicemodel.json
+#(Get-Content $servicemodel).replace("{{ServiceMetadata.ServiceGroup}}", $ServiceMetadata_ServiceGroup) | Set-Content $pathToRollout 
+#(Get-Content $servicemodel).replace("{{RollOutMetadata.Name}}", $RollOutMetadata_Name) | Set-Content $servicemodel 
+#(Get-Content $servicemodel).replace("{{RollOutMetadata.Name}}", $RollOutMetadata_Name) | Set-Content $servicemodel 
+#(Get-Content $servicemodel).replace("{{RollOutMetadata.Name}}", $RollOutMetadata_Name) | Set-Content $servicemodel 
+#(Get-Content $servicemodel).replace("{{RollOutMetadata.Name}}", $RollOutMetadata_Name) | Set-Content $servicemodel 
+#(Get-Content $servicemodel).replace("{{RollOutMetadata.Name}}", $RollOutMetadata_Name) | Set-Content $servicemodel 
+#(Get-Content $servicemodel).replace("{{RollOutMetadata.Name}}", $RollOutMetadata_Name) | Set-Content $servicemodel 
+#(Get-Content $servicemodel).replace("{{RollOutMetadata.Name}}", $RollOutMetadata_Name) | Set-Content $servicemodel 
+
+
 
 $rollout | ConvertTo-Json | set-content $pathToRollout;
 $servicemodel | ConvertTo-Json | set-content $pathtoServiceModel;
